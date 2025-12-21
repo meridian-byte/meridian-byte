bcae09ff19e227d2ab52bf427835fcb374f9b90d
Kevon <dev@pro.kevon.net>
2025-12-21 10:25:40 +0300

    refactor: move partials to one component refactor: drill diary date value to serving action components feat: add previous day checker helper

--- apps/client-sate/src/app/(app)/app/diary/page.tsx
+++ apps/client-sate/src/app/(app)/app/diary/page.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Metadata } from 'next';
 import LayoutPage from '@repo/components/layout/page';
-import LayoutHeadersDiary from '@/components/layout/headers/diary';
-import PartialOverviewDiary from '@/components/partial/overview/diary';
 import PartialPageAppDiary from '@/components/partial/page/app/diary';
 
 export const metadata: Metadata = { title: 'Diary' };
@@ -10,10 +8,6 @@ export const metadata: Metadata = { title: 'Diary' };
 export default function Diary() {
   return (
     <LayoutPage>
-      <LayoutHeadersDiary />
-
-      <PartialOverviewDiary />
-
       <PartialPageAppDiary />
     </LayoutPage>
   );
