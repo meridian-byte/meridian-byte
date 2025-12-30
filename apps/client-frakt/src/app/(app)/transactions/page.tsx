93bfa51bf8dfda69800487e5c89bc0c98790228a
Kevon <dev@pro.kevon.net>
2025-12-30 14:33:15 +0300

    feat: add entry date support refactor: relocate entry date hook to package refactor: update transfer logic to exclude balances from totals

--- apps/client-frakt/src/app/(app)/transactions/page.tsx
+++ apps/client-frakt/src/app/(app)/transactions/page.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Metadata } from 'next';
 import LayoutPage from '@repo/components/layout/page';
-import LayoutHeadersTransactions from '@/components/layout/headers/transactions';
-import PartialOverviewTransactions from '@/components/partial/overview/transactions';
 import PartialPageAppTransactions from '@/components/partial/page/app/transactions';
 
 export const metadata: Metadata = { title: 'Transactions' };
@@ -10,10 +8,6 @@ export const metadata: Metadata = { title: 'Transactions' };
 export default function Transactions() {
   return (
     <LayoutPage>
-      <LayoutHeadersTransactions />
-
-      <PartialOverviewTransactions />
-
       <PartialPageAppTransactions />
     </LayoutPage>
   );
