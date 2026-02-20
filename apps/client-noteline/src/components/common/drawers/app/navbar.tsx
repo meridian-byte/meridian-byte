a6fb77b0501f53f8c17e3db94709309ec8c6cdf3
Kevon <dev@pro.kevon.net>
2026-02-20 13:49:58 +0300

    fix: conditional drawer rendering

--- apps/client-noteline/src/components/common/drawers/app/navbar.tsx
+++ apps/client-noteline/src/components/common/drawers/app/navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { ActionIcon, Drawer, Group } from '@mantine/core';
+import { Box, ActionIcon, Drawer, Group } from '@mantine/core';
 import { useStoreAppShell } from '@repo/libraries/zustand/stores/shell';
 import { AppShell } from '@repo/types/components';
 import { COOKIE_NAME } from '@repo/constants/names';
@@ -50,33 +50,34 @@ export default function Navbar({ children }: { children: React.ReactNode }) {
 
   return (
     <>
-      <Drawer
-        hiddenFrom="xs"
-        keepMounted
-        opened={appshell?.child.navbar ?? false}
-        padding={0}
-        withCloseButton={false}
-        onClose={handleClose}
-        style={{ display: !mobile ? 'none' : undefined }}
-        styles={{
-          content: {
-            backgroundColor:
-              'light-dark(var(--mantine-color-gray-0), var(--mantine-color-dark-8))',
-          },
-        }}
-      >
-        <Group p={'xs'}>
-          <ActionIcon
-            size={ICON_WRAPPER_SIZE}
-            variant={'transparent'}
-            onClick={handleClose}
-          >
-            <IconX size={ICON_SIZE} stroke={ICON_STROKE_WIDTH} />
-          </ActionIcon>
-        </Group>
+      {mobile && (
+        <Drawer
+          hiddenFrom="xs"
+          keepMounted
+          opened={appshell?.child.navbar ?? false}
+          padding={0}
+          withCloseButton={false}
+          onClose={handleClose}
+          styles={{
+            content: {
+              backgroundColor:
+                'light-dark(var(--mantine-color-gray-0), var(--mantine-color-dark-8))',
+            },
+          }}
+        >
+          <Group p={'xs'}>
+            <ActionIcon
+              size={ICON_WRAPPER_SIZE}
+              variant={'transparent'}
+              onClick={handleClose}
+            >
+              <IconX size={ICON_SIZE} stroke={ICON_STROKE_WIDTH} />
+            </ActionIcon>
+          </Group>
 
-        <TabNavbarLeft />
-      </Drawer>
+          <TabNavbarLeft />
+        </Drawer>
+      )}
 
       <span
         onClick={() => {
