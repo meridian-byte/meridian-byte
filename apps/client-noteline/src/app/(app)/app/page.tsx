d0fd4942a18d83018be51a58c31a4bbf69498777
Kevon <dev@pro.kevon.net>
2026-03-24 16:31:04 +0300

    refactor: update Home component to utilize PartialPageHome and enhance layout with recent notes and quick note features

--- apps/client-noteline/src/app/(app)/app/page.tsx
+++ apps/client-noteline/src/app/(app)/app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Metadata } from 'next';
+import PartialPageHome from '@/components/partial/page/home';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
 
 export default function Home() {
-  return <div>home</div>;
+  return <PartialPageHome />;
 }
--- apps/client-noteline/src/components/partial/page/home.tsx
+++ apps/client-noteline/src/components/partial/page/home.tsx
@@ -1,13 +1,130 @@
 'use client';
 
-import React from 'react';
-import { SECTION_SPACING } from '@repo/constants/sizes';
-import { Container } from '@mantine/core';
+import React, { useMemo } from 'react';
+import {
+  ICON_SIZE,
+  ICON_STROKE_WIDTH,
+  SECTION_SPACING,
+} from '@repo/constants/sizes';
+import {
+  Box,
+  Button,
+  Card,
+  CardSection,
+  Center,
+  Container,
+  Divider,
+  Group,
+  SimpleGrid,
+  Stack,
+  Text,
+  Textarea,
+  Title,
+  Tooltip,
+} from '@mantine/core';
+import LayoutSection from '@repo/components/layout/section';
+import { useStoreNote } from '@repo/libraries/zustand/stores/note';
+import { NoteGet } from '@repo/types/models/note';
+import { IconEdit, IconHistory, IconNote } from '@tabler/icons-react';
+import { getRegionalDate } from '@repo/utilities/date-time';
+import { sortArray } from '@repo/utilities/array';
+import { Order } from '@repo/types/enums';
+import { linkify } from '@repo/utilities/url';
+import Link from 'next/link';
+import { useField } from '@mantine/form';
+import EditorMain from '@repo/components/common/editors/main';
 
 export default function Home() {
+  const notes = useStoreNote((s) => s.notes);
+
   return (
-    <Container py={SECTION_SPACING / 2}>
-      <div>home</div>
-    </Container>
+    <LayoutSection id={`app-home`} padded containerized={'md'}>
+      <Stack gap={SECTION_SPACING}>
+        <Stack>
+          <Group gap={5} c={'dimmed'}>
+            <IconEdit size={ICON_SIZE - 4} stroke={ICON_STROKE_WIDTH} />
+
+            <Title order={2} fz={'sm'}>
+              Quick Note
+            </Title>
+          </Group>
+
+          <Box>
+            <EditorMain />
+          </Box>
+        </Stack>
+
+        <Stack>
+          <Group gap={5} c={'dimmed'}>
+            <IconHistory size={ICON_SIZE - 4} stroke={ICON_STROKE_WIDTH} />
+
+            <Title order={2} fz={'sm'}>
+              Recent Notes
+            </Title>
+          </Group>
+
+          <SimpleGrid cols={{ base: 2, xs: 3, sm: 4, md: 3, lg: 4, xl: 5 }}>
+            {sortArray(notes || [], (i) => i.updated_at, Order.DESCENDING).map(
+              (ni, i) =>
+                i < 6 && (
+                  <div key={ni.id}>
+                    <RecentNoteCard props={ni} />
+                  </div>
+                )
+            )}
+          </SimpleGrid>
+        </Stack>
+      </Stack>
+    </LayoutSection>
+  );
+}
+
+function RecentNoteCard({ props }: { props: NoteGet }) {
+  const link = useMemo(() => {
+    return `/app/n/${linkify(props.title || '')}-${props.id}`;
+  }, [props.title, props.id]);
+
+  const timeStamp = getRegionalDate(props.updated_at);
+
+  return (
+    <Link href={link}>
+      <Card
+        bg={
+          'light-dark(var(--mantine-color-gray-1), var(--mantine-color-dark-8))'
+        }
+      >
+        <CardSection
+          p={'xs'}
+          bg={
+            'light-dark(var(--mantine-color-gray-2), var(--mantine-color-dark-7))'
+          }
+          c={'dimmed'}
+        >
+          <Group h={40} align="start">
+            <IconNote size={ICON_SIZE * 1.5} stroke={ICON_STROKE_WIDTH} />
+          </Group>
+        </CardSection>
+
+        <CardSection p={'xs'}>
+          <Stack gap={'xs'}>
+            <Tooltip
+              label={props.title}
+              multiline
+              maw={200}
+              position="top-start"
+              arrowOffset={16}
+            >
+              <Title order={3} fz={'sm'} fw={500} lineClamp={1}>
+                {props.title}
+              </Title>
+            </Tooltip>
+
+            <Text fz={'xs'} c={'dimmed'}>
+              {timeStamp.date}, {timeStamp.time}
+            </Text>
+          </Stack>
+        </CardSection>
+      </Card>
+    </Link>
   );
 }
