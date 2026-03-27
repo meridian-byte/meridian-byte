bd7db1acd4ad6a71d032821ab61bb8f31e1f9ee5
Kevon <work.kevon@gmail.com>
2026-03-27 15:35:40 +0300

    chore: add comment

--- apps/client-sate/src/hooks/nutrients.ts
+++ apps/client-sate/src/hooks/nutrients.ts
@@ -68,6 +68,8 @@ export const useMealTotals = (params: { meal: MealGet }) => {
 
 export const useEatTotals = (params: { eats: EatGet[] }) => {
   const { eats } = params;
+
+  // remove duplicates
   const dedupedEats = Array.from(new Map(eats.map((e) => [e.id, e])).values());
 
   const { servings } = useStoreServing();
