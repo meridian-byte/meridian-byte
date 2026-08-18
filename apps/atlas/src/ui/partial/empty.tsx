ac86cb12a7e624fb49e293a5a39a0acde69bae08
kevon <dev@kevon.net>
2026-08-18 16:38:08 +0300

    feat: add loading state to Empty component with conditional rendering

--- apps/atlas/src/ui/partial/empty.tsx
+++ apps/atlas/src/ui/partial/empty.tsx
@@ -1,20 +1,26 @@
 'use client';
 
 import React from 'react';
-import { Group, Stack, Text, ThemeIcon } from '@mantine/core';
+import { Center, Group, Loader, Stack, Text, ThemeIcon } from '@mantine/core';
 import { ICON_SIZE, ICON_STROKE_WIDTH, ICON_WRAPPER_SIZE, SECTION_SPACING } from '@repo/constants';
 import { IconMoodEmpty } from '@tabler/icons-react';
 
-export default function Empty({ label }: { label?: string }) {
+export default function Empty({ label, loading }: { label?: string; loading?: boolean }) {
+  const sharedSize = ICON_WRAPPER_SIZE * 1.5;
+
   return (
     <Stack align="center" py={SECTION_SPACING}>
-      <Group>
-        <ThemeIcon size={ICON_WRAPPER_SIZE * 1.5} variant="default">
+      <Center h={sharedSize} w={sharedSize} display={loading ? undefined : 'none'}>
+        <Loader />
+      </Center>
+
+      <Group display={loading ? 'none' : undefined}>
+        <ThemeIcon size={sharedSize} variant="default">
           <IconMoodEmpty size={ICON_SIZE * 1.5} stroke={ICON_STROKE_WIDTH} />
         </ThemeIcon>
       </Group>
 
-      <Text fz={'sm'} c={'dimmed'}>
+      <Text fz={'sm'} c={'dimmed'} ta={'center'} display={loading ? 'none' : undefined}>
         {label || 'Empty.'}
       </Text>
     </Stack>
